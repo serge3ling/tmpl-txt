9e05f01e91a0471b78209cffd1be0ee4c959ce55
srg3trt <stretiachenko@sompo-intl.com>
2025-07-22 13:18:01 +0300

    refactor: IdInc to extend abstract; comment how to use many id getters

--- App.java
+++ App.java
@@ -3,10 +3,16 @@ public class App {
     String appName = "Template Text App";
     System.out.println(appName + " started.");
 
+/* // This is how many id getters can be used.
+    List<IdGet> idGets = new ArrayList<>();
+    idGets.add(new IdGetFile());
+    idGets.add(new IdGetFile("id2.txt", new IdInc(4)));
+    idGets.add(new IdGetFile("id3.txt", new IdInc(4)));
+*/
     OverallWriter writer =
         (args.length > 0) ?
         new OverallWriter(args[0]) :
-        new OverallWriter(new Cnf().trgDir());
+        new OverallWriter(new Cnf().trgDir()/*, idGets*/);
     
     writer.write();
 
